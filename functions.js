c3ce09e52ef05a5550e5ee569c1cce50f8c374a5
Steve Larsen <stevelarsen01@gmail.com>
2017-05-11 13:54:54 -0500

    initial commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+stickOrChopsticks=(things,n)=>//coding and coding.. shorter and more shorter..  good luck! ;-)
+
+
+console.log(stickOrChopsticks("stick",10),5);
+console.log(stickOrChopsticks("stick",11),-1);
+console.log(stickOrChopsticks("chopstick",5),10);
+
+})})
