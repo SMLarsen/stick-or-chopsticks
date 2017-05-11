896cc71adcc71746090f145b1bd4b17b54d93d55
Steve Larsen <stevelarsen01@gmail.com>
2017-05-11 14:05:34 -0500

    solved but 75 characters not < 44

--- functions.js
+++ functions.js
@@ -1,9 +1,10 @@
 /*jshint esversion: 6 */
-stickOrChopsticks=(things,n)=>//coding and coding.. shorter and more shorter..  good luck! ;-)
+stickOrChopsticks=(things,n)=>{
+  return things === "stick" ? ( (n % 2) ? -1 : n / 2) : n * 2;
+};
 
+console.log(('stickOrChopsticks=(things,n)=>{return things=="stick"?((n%2)?-1:n/2):n*2;};').length);
 
 console.log(stickOrChopsticks("stick",10),5);
 console.log(stickOrChopsticks("stick",11),-1);
 console.log(stickOrChopsticks("chopstick",5),10);
-
-})})
